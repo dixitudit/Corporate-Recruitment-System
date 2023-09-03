cbbc325cc8029aff665a473c54a70cd6aa611c7d
dixitudit <58696687+dixitudit@users.noreply.github.com>
2023-09-03 19:21:37 +0530

    footer edit

--- Client/src/components/Footer.jsx
+++ Client/src/components/Footer.jsx
@@ -1,40 +1,147 @@
+import { Link } from "react-router-dom";
 export default function Footer(){
 
     return (<>
-        <footer id="ContactUs" className="bg-sky-300 p-4 text-blue-950">
-            <span className="font-bold px-6 font-serif ">Contact Us</span>
-            <div className="md:items-center sm:px-12 px-4 py-3 ">
-                <ul>
-                    <li className=" py-1">
-                        <a href="https://goo.gl/maps/mp27N3bQBSAXym4D7" rel="noreferrer" target="_blank" className="flex gap-2">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 m-0">
-                                <path strokeLinecap="round" strokeLinejoin="round" d="M15 10.5a3 3 0 11-6 0 3 3 0 016 0z" />
-                                <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z" />
-                            </svg>
-                            <p>6C84+QC6, Maulana Azad National Institute of Technology, Bhopal, Madhya Pradesh 462007</p>
-                        </a>
-                    </li>
-                    <li className=" gap-2 py-1 flex">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                            <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 002.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 01-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 00-1.091-.852H4.5A2.25 2.25 0 002.25 4.5v2.25z" />
-                        </svg>
-                        <p>1800 0000 000</p>
-                    </li>
-                    <li className=" gap-2 flex py-1">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                            <path strokeLinecap="round" strokeLinejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75" />
-                        </svg>
-                        <p>innovatetrio@gmail.com</p>
-                    </li>
-                    <li className="py1">
-                        <a href="https://github.com/dixitudit/Corporate-Recruitment-System" rel="noreferrer" target="_blank" className="gap-2 flex ">
-                            <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="25" height="25" viewBox="0 0 30 30">
-                                <path d="M15,3C8.373,3,3,8.373,3,15c0,5.623,3.872,10.328,9.092,11.63C12.036,26.468,12,26.28,12,26.047v-2.051 c-0.487,0-1.303,0-1.508,0c-0.821,0-1.551-0.353-1.905-1.009c-0.393-0.729-0.461-1.844-1.435-2.526 c-0.289-0.227-0.069-0.486,0.264-0.451c0.615,0.174,1.125,0.596,1.605,1.222c0.478,0.627,0.703,0.769,1.596,0.769 c0.433,0,1.081-0.025,1.691-0.121c0.328-0.833,0.895-1.6,1.588-1.962c-3.996-0.411-5.903-2.399-5.903-5.098 c0-1.162,0.495-2.286,1.336-3.233C9.053,10.647,8.706,8.73,9.435,8c1.798,0,2.885,1.166,3.146,1.481C13.477,9.174,14.461,9,15.495,9 c1.036,0,2.024,0.174,2.922,0.483C18.675,9.17,19.763,8,21.565,8c0.732,0.731,0.381,2.656,0.102,3.594 c0.836,0.945,1.328,2.066,1.328,3.226c0,2.697-1.904,4.684-5.894,5.097C18.199,20.49,19,22.1,19,23.313v2.734 c0,0.104-0.023,0.179-0.035,0.268C23.641,24.676,27,20.236,27,15C27,8.373,21.627,3,15,3z"></path>
-                            </svg>
-                            <p>Open Source</p>
-                        </a>
-                    </li>
-                </ul>
+        <footer id="ContactUs" className="bg-sky-200 text-blue-950 pr-[50px] pl-[50px]">
+            
+            <div className="md:items-start max-md:text-center md:justify-between md:flex sm:px-12 px-4 py-4 ">
+                <div className="p-2">
+                    <span className="font-bold font-serif">Contact Us</span>
+                    <ul>
+                        <li className=" py-1">
+                            <a href="https://goo.gl/maps/mp27N3bQBSAXym4D7" rel="noreferrer" target="_blank" >
+                                <p className="flex gap-1 items-center max-md:justify-center">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 m-0">
+                                        <path strokeLinecap="round" strokeLinejoin="round" d="M15 10.5a3 3 0 11-6 0 3 3 0 016 0z" />
+                                        <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z" />
+                                    </svg>
+                                
+                                    Manit, Bhopal, Madhya Pradesh 462007
+                                </p>
+                            </a>
+                            
+                        </li>
+                        <li className="py-1">
+                            <p className="flex gap-1 items-center max-md:justify-center"> 
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 002.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 01-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 00-1.091-.852H4.5A2.25 2.25 0 002.25 4.5v2.25z" />
+                                </svg>
+                                1800 0000 000
+                            </p>
+                        </li>
+                        <li className="py-1">
+                            <p className="flex gap-1 items-center max-md:justify-center"> 
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75" />
+                                </svg>
+                                innovatetrio@gmail.com
+                            </p>
+                        </li>
+                        <li className="py-1">
+                            <a href="https://github.com/dixitudit/Corporate-Recruitment-System" rel="noreferrer" target="_blank">
+                            <p className="flex gap-1 items-center max-md:justify-center"> 
+                                <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="25" height="25" viewBox="0 0 30 30">
+                                    <path d="M15,3C8.373,3,3,8.373,3,15c0,5.623,3.872,10.328,9.092,11.63C12.036,26.468,12,26.28,12,26.047v-2.051 c-0.487,0-1.303,0-1.508,0c-0.821,0-1.551-0.353-1.905-1.009c-0.393-0.729-0.461-1.844-1.435-2.526 c-0.289-0.227-0.069-0.486,0.264-0.451c0.615,0.174,1.125,0.596,1.605,1.222c0.478,0.627,0.703,0.769,1.596,0.769 c0.433,0,1.081-0.025,1.691-0.121c0.328-0.833,0.895-1.6,1.588-1.962c-3.996-0.411-5.903-2.399-5.903-5.098 c0-1.162,0.495-2.286,1.336-3.233C9.053,10.647,8.706,8.73,9.435,8c1.798,0,2.885,1.166,3.146,1.481C13.477,9.174,14.461,9,15.495,9 c1.036,0,2.024,0.174,2.922,0.483C18.675,9.17,19.763,8,21.565,8c0.732,0.731,0.381,2.656,0.102,3.594 c0.836,0.945,1.328,2.066,1.328,3.226c0,2.697-1.904,4.684-5.894,5.097C18.199,20.49,19,22.1,19,23.313v2.734 c0,0.104-0.023,0.179-0.035,0.268C23.641,24.676,27,20.236,27,15C27,8.373,21.627,3,15,3z"></path>
+                                </svg>
+                                Open Source
+                            </p>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                <div className="p-2">
+                    <span className="font-bold font-serif">For You</span>
+                    <ul >
+                        <li className="px-2">
+                            <Link to="/">Home</Link>
+                        </li>
+                        <li className="px-2">
+                            <Link to="/Jobs">Jobs</Link>
+                        </li>
+                        <li className="px-2">
+                            <Link to="/Companies">Companies</Link>
+                        </li>
+                        <li className="px-2">
+                            <Link to="/Practice">Practice</Link>
+                        </li>
+                        <li className="px-2">
+                            <Link to="/login">Login</Link>
+                        </li>
+                    </ul>
+                </div>
+                <div className="p-2">
+                    <span className="font-bold font-serif">Follow Us</span>
+                    <ul >
+                        <li className="py-1">
+                            <Link to="/404">
+                                <p className="flex gap-1 items-center max-md:justify-center"> 
+                                        <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        className="w-4 h-4 m-0"
+                                        fill="currentColor"
+                                        viewBox="0 0 24 24">
+                                        <path
+                                            d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z" />
+                                        </svg>
+                                
+                                    Facebook
+                                    
+                                </p>
+                            </Link>
+                        </li>
+                        <li className="py-1">
+                            <Link to="404" >
+                                <p className="flex gap-1  items-center max-md:justify-center">
+                                <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                className="h-4 w-4 m-0"
+                                fill="currentColor"
+                                viewBox="0 0 24 24">
+                                <path
+                                    d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z" />
+                                </svg>
+                                Twitter
+                                </p>
+                            </Link>
+                        </li>
+                        <li className="py-1">
+                            <Link to="404" >
+                                <p className="flex gap-1 items-center max-md:justify-center">
+                                <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                className="h-4 w-4"
+                                fill="currentColor"
+                                viewBox="0 0 24 24">
+                                <path
+                                    d="M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zm0-2.163c-3.259 0-3.667.014-4.947.072-4.358.2-6.78 2.618-6.98 6.98-.059 1.281-.073 1.689-.073 4.948 0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98 1.281.058 1.689.072 4.948.072 3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98-1.281-.059-1.69-.073-4.949-.073zm0 5.838c-3.403 0-6.162 2.759-6.162 6.162s2.759 6.163 6.162 6.163 6.162-2.759 6.162-6.163c0-3.403-2.759-6.162-6.162-6.162zm0 10.162c-2.209 0-4-1.79-4-4 0-2.209 1.791-4 4-4s4 1.791 4 4c0 2.21-1.791 4-4 4zm6.406-11.845c-.796 0-1.441.645-1.441 1.44s.645 1.44 1.441 1.44c.795 0 1.439-.645 1.439-1.44s-.644-1.44-1.439-1.44z" />
+                                </svg>
+                                Instagram
+                                </p>
+                            </Link>
+                        </li>
+                        <li className="py-1">
+                            <Link to="404" >
+                            <p className="flex gap-1 items-center max-md:justify-center"> 
+                                <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                className="h-4 w-4 m-0"
+                                fill="currentColor"
+                                viewBox="0 0 24 24">
+                                <path
+                                    d="M4.98 3.5c0 1.381-1.11 2.5-2.48 2.5s-2.48-1.119-2.48-2.5c0-1.38 1.11-2.5 2.48-2.5s2.48 1.12 2.48 2.5zm.02 4.5h-5v16h5v-16zm7.982 0h-4.968v16h4.969v-8.399c0-4.67 6.029-5.052 6.029 0v8.399h4.988v-10.131c0-7.88-8.922-7.593-11.018-3.714v-2.155z" />
+                                </svg>
+                                Linkedin
+                                </p>
+                            </Link>
+                        </li>
+                        
+                    </ul>
+                </div>
+            </div>
+            <hr />
+            <div className="p-1 text-center">
+                <span>© 2023 Copyright:</span>
+                <p className="font-semibold">Skill Magnet</p>
             </div>
         </footer>
     </>);
