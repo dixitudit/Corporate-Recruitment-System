81d1b7f5a3da94f582eefd9a507f4ac35e45b438
may-ag <mayag@gmail.com>
2023-09-13 19:23:44 +0530

    login page again edited

--- Client/src/Pages/Login.jsx
+++ Client/src/Pages/Login.jsx
@@ -49,7 +49,7 @@ import { useState  } from "react"
         <div class="mt-2">
           
           <select name="User Role"  id="User Role" required className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-200 sm:text-sm sm:leading-6">
-            <option value="Select">Select</option>
+            <option value="">Select</option>
             <option value="Recruiter">Login As Recruiter</option>
             <option value="Candidate">Login As Candidate</option>
           </select>
