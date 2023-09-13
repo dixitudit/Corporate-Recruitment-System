1f4a4a57f3cf7161d35cda2327994bf7313ae11d
Chahat Gupta <chahatguptanit@gmail.com>
2023-09-13 19:07:23 +0530

    add icons on for us

--- Client/src/components/Footer.jsx
+++ Client/src/components/Footer.jsx
@@ -6,7 +6,7 @@ export default function Footer(){
             
             <div className="md:items-start max-md:text-center md:justify-between md:flex sm:px-12 px-4 py-4 ">
                 <div className="p-2 mb-2">
-                    <span className="font-bold font-serif">Contact Us</span>
+                    <span className="font-bold font-serif underline ">Contact Us</span>
                     <ul>
                         <li className=" py-1">
                             <a href="https://goo.gl/maps/mp27N3bQBSAXym4D7" rel="noreferrer" target="_blank" >
@@ -50,27 +50,75 @@ export default function Footer(){
                     </ul>
                 </div>
                 <div className="p-2 mb-2">
-                    <span className="font-bold font-serif">For You</span>
+                    <span className="font-bold font-serif ml-2 underline ">For You</span>
                     <ul >
                         <li className="px-2">
-                            <Link to="/">Home</Link>
+                            <Link to="/">
+                            <p className="flex gap-1 items-center max-md:justify-center">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
+                            </svg>
+
+
+                            Home
+                            </p>
+                            </Link>
+                            
                         </li>
                         <li className="px-2">
-                            <Link to="/Jobs">Jobs</Link>
+                            <Link to="/Jobs">
+                            <p className="flex gap-1 items-center max-md:justify-center">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M20.25 14.15v4.25c0 1.094-.787 2.036-1.872 2.18-2.087.277-4.216.42-6.378.42s-4.291-.143-6.378-.42c-1.085-.144-1.872-1.086-1.872-2.18v-4.25m16.5 0a2.18 2.18 0 00.75-1.661V8.706c0-1.081-.768-2.015-1.837-2.175a48.114 48.114 0 00-3.413-.387m4.5 8.006c-.194.165-.42.295-.673.38A23.978 23.978 0 0112 15.75c-2.648 0-5.195-.429-7.577-1.22a2.016 2.016 0 01-.673-.38m0 0A2.18 2.18 0 013 12.489V8.706c0-1.081.768-2.015 1.837-2.175a48.111 48.111 0 013.413-.387m7.5 0V5.25A2.25 2.25 0 0013.5 3h-3a2.25 2.25 0 00-2.25 2.25v.894m7.5 0a48.667 48.667 0 00-7.5 0M12 12.75h.008v.008H12v-.008z" />
+                            </svg>
+
+
+                            Jobs
+                            </p>
+                            </Link>
+                            
                         </li>
                         <li className="px-2">
-                            <Link to="/Companies">Companies</Link>
+                            <Link to="/Companies">
+                            <p className="flex gap-1 items-center max-md:justify-center">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 21h16.5M4.5 3h15M5.25 3v18m13.5-18v18M9 6.75h1.5m-1.5 3h1.5m-1.5 3h1.5m3-6H15m-1.5 3H15m-1.5 3H15M9 21v-3.375c0-.621.504-1.125 1.125-1.125h3.75c.621 0 1.125.504 1.125 1.125V21" />
+                            </svg>
+
+
+                            Companies
+                            </p>
+                            </Link>
+                            
                         </li>
                         <li className="px-2">
-                            <Link to="/Practice">Practice</Link>
+                            <Link to="/Practice">
+                            <p className="flex gap-1 items-center max-md:justify-center">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M9 17.25v1.007a3 3 0 01-.879 2.122L7.5 21h9l-.621-.621A3 3 0 0115 18.257V17.25m6-12V15a2.25 2.25 0 01-2.25 2.25H5.25A2.25 2.25 0 013 15V5.25m18 0A2.25 2.25 0 0018.75 3H5.25A2.25 2.25 0 003 5.25m18 0V12a2.25 2.25 0 01-2.25 2.25H5.25A2.25 2.25 0 013 12V5.25" />
+                            </svg>
+
+
+                            Practice
+                            </p>
+                            </Link>
+                            
                         </li>
                         <li className="px-2">
-                            <Link to="/login">Login</Link>
+                            <Link to="/login">
+                            <p className="flex gap-1 items-center max-md:justify-center">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15m3 0l3-3m0 0l-3-3m3 3H9" />
+                            </svg>
+                            
+                            Login
+                            </p>
+                            </Link>
                         </li>
                     </ul>
                 </div>
                 <div className="p-2">
-                    <span className="font-bold font-serif">Follow Us</span>
+                    <span className="font-bold font-serif underline">Follow Us</span>
                     <ul >
                         <li className="py-1">
                             <Link to="/404">
